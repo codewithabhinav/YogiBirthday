f022af986479d3f840c446327b530aa1d158a565
Abhinav kumar <presonalabhi@gmail.com>
2020-09-02 23:26:34 +0530

    first commit in this repo

---
+++ main.js
@@ -0,0 +1,38 @@
+// TODO:- don't use this code this is code not working properly;
+
+
+// const names = new Array(
+
+//     {
+//         name: "Bro"
+//     }, {
+//         name: "yogi"
+//     }, {
+//         name: " Topper"
+//     }, {
+//         name: "small Bank"
+//     },
+
+// );
+
+
+// function nameGenerator() {
+//     const naam = document.querySelector(".generate");
+//     naam.innerText = "";
+
+//     console.log("function running");
+
+//     for (let i = 0; i < names.length; i++) {
+
+
+//         console.log("for loop runing");
+//         const na = document.createTextNode(names.name);
+
+//         naam.appendChild(na)
+
+
+//     };
+// }
+
+
+// // nameGenerator();
